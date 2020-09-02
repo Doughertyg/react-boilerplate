a21642f6e4092d3a48f935a0bcf966bc284189d7
doughertyg <doughertyg37@gmail.com>
2020-09-02 16:08:13 -0700

    setup viewer actions and action types

--- app/containers/Viewer/actions.js
+++ app/containers/Viewer/actions.js
@@ -4,10 +4,51 @@
  *
  */
 
-import { DEFAULT_ACTION } from './constants';
+import {
+  GET_STRINGLIST,
+  SET_STRINGLIST,
+  SET_STRINGLIST_ERROR,
+  SET_LOADING,
+} from './constants';
 
-export function defaultAction() {
+/**
+ * action that triggers saga to fetch string list
+ */
+export function getStringList() {
   return {
-    type: DEFAULT_ACTION,
+    type: GET_STRINGLIST,
+  };
+}
+
+/**
+ * action that updates the redux store with fetched string list
+ * @param {Array} stringlist array of string
+ */
+export function setStringList(stringlist) {
+  return {
+    type: SET_STRINGLIST,
+    stringlist,
+  };
+}
+
+/**
+ * action that sets the error message in the redux store
+ * @param {string} err error message
+ */
+export function setStringListError(err) {
+  return {
+    type: SET_STRINGLIST_ERROR,
+    err,
+  };
+}
+
+/**
+ * action to set the loading state in redux
+ * @param {boolean} isLoading bool value respresenting if currently loading
+ */
+export function setLoading(isLoading) {
+  return {
+    type: SET_LOADING,
+    isLoading,
   };
 }
--- app/containers/Viewer/constants.js
+++ app/containers/Viewer/constants.js
@@ -4,4 +4,7 @@
  *
  */
 
-export const DEFAULT_ACTION = 'app/Viewer/DEFAULT_ACTION';
+export const GET_STRINGLIST = 'app/Viewer/GET_STRINGLIST';
+export const SET_STRINGLIST = 'app/Viewer/SET_STRINGLIST';
+export const SET_STRINGLIST_ERROR = 'app/Viewer/SET_STRINGLIST_ERROR';
+export const SET_LOADING = 'app/Viewer/SET_LOADING';
