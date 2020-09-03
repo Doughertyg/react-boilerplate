1d6b3f38fc1937ff7452a5803dfdc3bd6705a357
doughertyg <doughertyg37@gmail.com>
2020-09-02 18:54:26 -0700

    style list and list items

---
+++ app/components/List/Li.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Li = styled.li`
+  list-style: none;
+  margin-bottom: 10px;
+`;
+
+export default Li;
--- app/components/List/index.js
+++ app/components/List/index.js
@@ -8,14 +8,14 @@ import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 
 import Wrapper from './Wrapper';
-import Ul from './Ul';
+import Li from './Li';
 
 function List({ items }) {
   // eslint-disable-next-line react/no-array-index-key
-  const content = items.map((item, index) => <li key={index}>{item}</li>);
+  const content = items.map((item, index) => <Li key={index}>{item}</Li>);
   return (
     <Wrapper>
-      <Ul>{content}</Ul>
+      <ul>{content}</ul>
     </Wrapper>
   );
 }
