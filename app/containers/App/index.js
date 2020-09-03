d374d273df5e2888b01a4814feb3c5be9caf6dc3
doughertyg <doughertyg37@gmail.com>
2020-09-02 18:01:28 -0700

    update App to use Viewer route instead of Homepage

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -10,20 +10,20 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import HomePage from 'containers/HomePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
+import Viewer from 'containers/Viewer/Loadable';
 
-import appWrapper from './appWrapper';
+import AppWrapper from './AppWrapper';
 import GlobalStyle from '../../global-styles';
 
 export default function App() {
   return (
-    <appWrapper>
+    <AppWrapper>
       <Switch>
-        <Route exact path="/" component={HomePage} />
+        <Route exact path="/" component={Viewer} />
         <Route component={NotFoundPage} />
       </Switch>
       <GlobalStyle />
-    </appWrapper>
+    </AppWrapper>
   );
 }
